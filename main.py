09766ff8fe27a2802207088e6925f8d666a41043
SergTsch87 <sergtsch87@gmail.com>
2026-03-25 23:48:04 +0200

    Upd main.py: Add init_game(), game_loop()

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+def init_game():
+    # Гравці розставляють кораблі.
+    # Визначається, хто ходить першим.
+    pass
+
+
+def game_loop():
+    pass
+
 
 def main():
     pass
