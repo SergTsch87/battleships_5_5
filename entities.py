e088c473b34f4e9546cd8896e09f0437cc85f78b
SergTsch87 <sergtsch87@gmail.com>
2026-03-26 23:55:00 +0200

    Upd entities.py, main.py: Add .Game, Del .Shot, Add comments

--- entities.py
+++ entities.py
@@ -1,13 +1,26 @@
 # Entities:
+    # Game
     # Field
     # Ship
     # Gamer
-    # Shot
+
+# Стан гри:
+#   Додайте центральний клас Game, який керує двома гравцями,
+#   чергою ходів і перевіркою перемоги. Це відокремить логіку
+#   від класів сутностей.
+
+class Game:
+    def __init__(self, Gamer):
+        pass
+
 
 class Field:
     def __init__(self, rows, cols):
         self.rows = rows
         self.cols = cols
+    
+    # клас повинен зберігати стан поля
+    #   (де розташовані кораблі, де були постріли)
 
 
 class Ship:
@@ -17,13 +30,19 @@ class Ship:
 
 
 class Gamer:
-    def __init__(self):
+    def __init__(self, name, state_step):
+        self.name = name
+        self.state_step = state_step # булевий прапорець (is_turn) або лічильник, щоб керувати чергою
+
+        # список кораблів (як екземпляри Ship)
+        # власне поле (екземпляр Field)
+        # можливо, список пострілів / історію ходів
+        
         # список кораблів певного гравця
         # стан поточного та наступного ходу: 0 / 1
-        pass
+        
 
-
-class Shot:
-    def __init__(self, cell, state):
-        self.cell = cell
-        self.state = state # Чи це тут треба?..
+# class Shot:
+#     def __init__(self, cell, state):
+#         self.cell = cell
+#         self.state = state # Чи це тут треба?..
